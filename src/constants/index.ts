5a93f410ded6f2ddaa5960bef3b5c94e7edc9253
Siarhei Buiko <innxfect@gmail.com>
2025-11-26 10:32:13 +0300

    JST-2585 feat(constants): add button label, title text, placeholder

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const ButtonLabel = {
+  LOGIN: 'Log in',
+  SUBMIT: 'Submit',
+};
+
+export const TittleText = {
+  LOGIN: 'Login',
+};
+
+export const Placeholder = {
+  EMAIL: 'example@gmail.com',
+  PASSWORD: '*********',
+};
