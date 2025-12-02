332c3f00aa5c05021306ce977f12105fc6346b90
Siarhei Buiko <innxfect@gmail.com>
2025-12-02 19:57:43 +0300

    JST-2585 feat(project): add store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
-import { theme } from '@assets/theme';
 import { ThemeProvider } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { theme } from '@/assets/theme';
+import { setupStore } from '@/store';
 
 import App from './App';
+
+const store = setupStore();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </StrictMode>,
 );
