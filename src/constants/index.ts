4fb67b4e447ba9cb35242ee98a3703057121db9f
Siarhei Buiko <innxfect@gmail.com>
2025-12-04 19:58:17 +0300

    JST-2585 feat(constants): add characters

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export const ButtonLabel = {
   LOGIN: 'Log in',
   LOGOUT: 'Log out',
   SUBMIT: 'Submit',
+  SEARCH: 'Search',
 };
 
 export const TittleText = {
@@ -11,8 +12,21 @@ export const TittleText = {
 export const Placeholder = {
   USERNAME: 'example',
   PASSWORD: '*********',
+  CHARNAME: 'Rick Sanchez',
 };
 
 export const LocalStorageKeys = {
   TOKEN: 'auth-token',
+};
+
+export const CHAR = {
+  STATUS: 'Status:',
+  SPECIES: 'Species:',
+  GENDER: 'Gender:',
+  ORIGIN: 'Origin:',
+  LOCATION: 'Location:',
+};
+
+export const EMPTY_LIST = {
+  CHAR_TEXT: 'Please enter a different Char name and click "Search"',
 };
