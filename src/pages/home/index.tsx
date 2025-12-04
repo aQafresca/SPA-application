d3af0acae282083b52c7e8e9f740a25185f197e7
Siarhei Buiko <innxfect@gmail.com>
2025-12-04 20:06:54 +0300

    JST-2585 feat(pages): add home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,49 @@
+import { Stack } from '@mui/material';
+import React, { useCallback, useState } from 'react';
+
+import { CardList } from '@/components/cards-list';
+import { SearchBar } from '@/components/search-bar';
+import { Placeholder } from '@/constants';
+import { useAppDispatch, useAppSelector } from '@/core/hooks/useRedux';
+import { setFilterName, setPage } from '@/store/characters';
+
 const HomePage = () => {
-  return <div>Home</div>;
+  const [charactersName, setCharactersName] = useState<string>('');
+  const dispatch = useAppDispatch();
+  const currentPage = useAppSelector((state) => state.characters.currentPage);
+  const searchTerm = useAppSelector((state) => state.characters.filterName);
+
+  const handleNameChange = useCallback((value: string) => {
+    setCharactersName(value);
+  }, []);
+
+  const handleSearch = useCallback(
+    (event: React.FormEvent) => {
+      event.preventDefault();
+      dispatch(setFilterName(charactersName));
+    },
+    [charactersName, dispatch],
+  );
+
+  const handlePageChange = useCallback(
+    (value: number) => {
+      dispatch(setPage(value));
+    },
+    [dispatch],
+  );
+
+  return (
+    <Stack component={'section'} alignItems="center" gap={4}>
+      <SearchBar
+        label={'Character'}
+        value={charactersName}
+        onChange={handleNameChange}
+        onSearch={handleSearch}
+        placeholder={Placeholder.CHARNAME}
+      />
+      <CardList currentPage={currentPage} onPageChange={handlePageChange} filterName={searchTerm} />
+    </Stack>
+  );
 };
 
 export default HomePage;
