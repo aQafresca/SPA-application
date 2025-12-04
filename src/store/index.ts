153d2a75f4d272e15c8a337f9d9589e4376eb38d
Siarhei Buiko <innxfect@gmail.com>
2025-12-04 19:59:05 +0300

    JST-2585 feat(store): add characters

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,23 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import { authApi } from '@/services/authApi';
+import { getCharactersApi } from '@/services/charactersApi';
 
 import authReducer from './auth';
+import charactersReducer from './characters';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  characters: charactersReducer,
   [authApi.reducerPath]: authApi.reducer,
+  [getCharactersApi.reducerPath]: getCharactersApi.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(authApi.middleware, getCharactersApi.middleware),
   });
 };
 
